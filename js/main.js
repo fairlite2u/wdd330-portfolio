128af5bbaf5da15fc72e38416bacc2fa46cbffd3
fairlite2u <pyl95001@byui.edu>
2021-04-23 15:58:59 -0600

    week 1 update

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const weekLinks = [
+    {label: "Week 1", url: "week1/index.html"}//,
+    //{label: "Week 2", url: "week2/index.html"}
+]
+
+let weekList = document.getElementById('weeks');
+
+for (let i = 0; i < weekLinks.length; i++) {
+    let week = document.createElement('li');
+    let a = document.createElement('a');
+    let weekLink = document.createTextNode(weekLinks[i].label);
+    a.appendChild(weekLink);
+    a.href = weekLinks[i].url;
+    week.appendChild(a);
+    weekList.appendChild(week);  
+}
