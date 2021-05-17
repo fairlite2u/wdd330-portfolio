12ea3efcf26d6c981194f832af289f8e5f71d1a3
fairlite2u <pyl95001@byui.edu>
2021-05-17 10:13:59 -0600

    Week 4 ch8

--- week4/chapter8/main.js
+++ week4/chapter8/main.js
@@ -1,32 +1,93 @@
-const input = form.elements.searchInput;
+// assign form to variable
+const form = document.forms['hero'];
 
-// input.addEventListener('focus', () => alert('focused'), false);
-// input.addEventListener('blur', () => alert('blurred'), false);
-// input.addEventListener('change', () => alert('changed'), false);
+// add event listener for when the form is submitted
+form.addEventListener('submit', makeHero, false);
+document.forms.hero.powers[0].checked = true;
 
-// input.value = 'Search Here';
-input.addEventListener('focus', function() {
-    if (input.value === 'Search Here') {
-        input.value = '';
+
+// Function to return an object based on the information provided in the form
+function makeHero(event) {
+    event.preventDefault(); // prevent the form from being submitted
+    const hero = {}; // create an empty object
+    hero.name = form.heroName.value; // create a name property based on the input field's value
+    hero.realName = form.realName.value; // values from a password input field are accessed in the the same way as text input fields
+    // hero.powers = []; // create a powers property for the hero object that starts as an empty array
+    // // iterate over each checkbox to see if it was checked in the form, use push method to add to array if checked
+    // for (let i = 0; i < form.powers.length; i++) {
+    //     if (form.powers[i].checked) {
+    //         hero.powers.push(form.powers[i].value);
+    //     }
+    // }
+
+    // Refactored to be more succint using array iterators
+    hero.powers = [...form.powers].filter(box => box.checked).map(box => box.value);
+
+    hero.category = form.category.value; // Access all the radio buttons in the group that has the same name and get value
+    hero.age = form.age.value; // Get age
+    hero.city = form.city.value; // Get city
+    hero.origin = form.origin.value; // Get text from text area
+    alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
+    return hero;
+}
+
+// // Example function to exclude any superhero names that begin with "X"
+// form.addEventListener('submit',validate,false);
+// function validate(event) {
+//     const firstLetter = form.heroName.value[0];
+//     if (firstLetter.toUpperCase() === 'X') {
+//         event.preventDefault();
+//         alert('Your name is not allowed to start with X!');
+//     }
+// }
+
+// Improved usability by giving instant feedback
+form.heroName.addEventListener('keyup',validateInline,false);
+const label = form.querySelector('label');
+const error = document.createElement('div');
+error.classList.add('error');
+error.textContent = '! Your name is not allowed to start with X.';
+label.append(error);
+
+function validateInline() {
+    const heroName = this.value.toUpperCase();
+    if(heroName.startsWith('X')){
+    error.style.display = 'block';
+    } else {
+    error.style.display = 'none';
     }
-}, false);
+}
 
-input.addEventListener('blur', function() {
-    if(input.value === '') {
-        input.value = 'Search Here';
-    } 
-}, false);
-const form = document.forms['search'];
-form.addEventListener('submit', search, false);
-
-// // function search() {
-// //     alert('Form Submitted');
-// //     event.preventDefault();
-// // }
-
-function search(event) {
-    alert(`You searched for: ${input.value}`);
-    event.preventDefault();
+// Function to disable the submit button is an input field is empty
+form.heroName.addEventListener('keyup',disableSubmit,false);
+function disableSubmit(event) {
+    if(event.target.value === ''){
+        document.getElementById('submit').disabled = true;
+    } else {
+        document.getElementById('submit').disabled = false;
+    }
 }
 
 
+
+
+// // JavaScript for search.html:
+// const form = document.forms['search'];
+// const input = form.elements.searchInput;
+// form.addEventListener ('submit', search, false);
+// function search() {
+//     alert(`You Searched for: ${input.value}`);
+//     event.preventDefault();
+// }
+// input.value = 'Search Here';
+// input.addEventListener('focus', function() {
+//     if (input.value==='Search Here') {
+//         input.value = ''
+//     }
+// }, false);
+
+// input.addEventListener('blur', function() {
+//     if (input.value==='') {
+//         input.value = 'Search Here';
+//     }
+// }, false);
---
+++ week4/quiz/main8.js
@@ -0,0 +1,131 @@
+// This part stays the same as the previous version
+const quiz = [
+    { name: "Superman",realName: "Clark Kent" },
+    { name: "Wonder Woman",realName: "Diana Prince" },
+    { name: "Batman",realName: "Bruce Wayne" },
+];
+
+// View Object - everything here is related to view in the html document
+const view = {
+    // Use document.querySelector and getElementById to connect to div in html
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    // Add reference to form in html
+    response: document.querySelector('#response'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    // Connect the button in the html to the js
+    start: document.getElementById('start'),
+    // the render function updates the content of an element on the page without reloading the page
+    // it uses a for loop to update HTML with the content provided
+    render(target,content,attributes) {
+        for(const key in attributes) {
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    },
+    // Add the show and hide functions to show and hide elements on the page
+    // These work by simply changing the style.display property to none to hide an element, and block to display it.
+    // These will make the start button disapper while the game is in progress and then reappear once the game is over.
+    show(element){
+        element.style.display = 'block';
+    },
+    hide(element){
+        element.style.display = 'none';
+    },
+    // This method resets te input field to an empty field and gives it focus
+    resetForm(){
+        this.response.answer.value = '';
+        // Focus improves usability because the player only has to concentrate on answering one question
+        this.response.answer.focus();
+    },
+    // This function uses the show and hide methods so the question, response, and results are visible and hides the start button
+    // The render method is used to reset the HTML content in the result and info elements to an empty string
+    // Also calls the new resetForm method
+    setup(){
+        this.show(this.question);
+        this.show(this.response);
+        this.show(this.result);
+        this.hide(this.start);
+        this.render(this.score,game.score);
+        this.render(this.result,'');
+        this.render(this.info,'');
+        this.resetForm();
+    },
+    // This method is called at the end of the game to hide any elements not required and make the start button visible again
+    teardown(){
+        this.hide(this.question);
+        this.hide(this.response);
+        this.show(this.start);
+    }
+};
+// Store questions as objects inside an array
+
+// Creat and object to be the namespace using the object literal pattern
+// this is similar to classes in Java that I am also learning about this week!
+const game = {
+    // modify the functions to be methods inside the object
+    start(quiz){
+        // the 'this' keyword refers to the object that is within
+        this.score = 0;
+        this.questions = [...quiz];
+        // The view.setup() method needs to be called at the beginning of every game
+        view.setup();
+        // add this.ask() so the first question will be asked
+        this.ask();       
+    },
+    // Update to add name object as parameter
+    ask(name){
+        // Check the length of the this.questions array to see if there are questions left
+        if(this.questions.length > 0) {
+            // If questions left, use pop() method to remove the last element in the array and assign it to this.question
+            this.question = this.questions.pop();
+            // The next two line of code stay the same from QuizNinja 7, but are placed inside the if statement
+            const question = `What is ${this.question.name}'s real name?`;
+            // the call to the render function replaces the prompt dialog
+            view.render(view.question,question);
+        }
+        // Stops asking questions if no questions left
+        else {
+            this.gameOver();
+        }
+    },
+    // Update to add event object as parameter
+    check(event){
+        // This next line prevents the form from being submitted
+        event.preventDefault();
+        // Get the answer that was submitted by user
+        const response = view.response.answer.value;
+        // This stays the same from last week
+        const answer = this.question.realName;
+        if(response === answer){
+        // the call to the render function will replace the alert dialog
+        view.render(view.result,'Correct!',{'class':'correct'});
+        alert('Correct!');
+        this.score++;
+        // the render function is inserted here to update the score as needed
+        view.render(view.score,this.score);
+        } 
+        else {
+        // the call to the render function will replace the alert dialog        
+        view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+        alert(`Wrong! The correct answer was ${answer}`);
+        }
+        // Add a call to the resetForm method
+        view.resetForm();
+        // Triggers the next question to be asked
+        this.ask();
+    },
+    gameOver(){
+        // the call to the render function replaces the alert dialog          
+        view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+        // Add the call to the hide function to unhide the start button
+        view.teardown();
+    }
+}
+
+// Update the call to the game.start function to be triggered by clicking the button
+view.start.addEventListener('click', () => game.start(quiz), false);
+// Add event listener that is triggered when the form is submitted
+view.response.addEventListener('submit', (event) => game.check(event), false);
+view.hide(view.response);
