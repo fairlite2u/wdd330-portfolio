68fdf33de60da8e723ffbf0a62252e564f96fac6
fairlite2u <pyl95001@byui.edu>
2021-05-29 13:29:06 -0600

    add todo link to home

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const weekLinks = [
     {label: "Week 2", url: "week2/index.html"},
     {label: "Week 3", url: "week3/index.html"}, 
     {label: "Week 4", url: "week4/index.html"},
-    {label: "Week 5", url: "week5/index.html"}
+    {label: "Week 5", url: "week5/index.html"},
+    {label: "Midterm: Todo App", url: "todo/index.html"}
 ]
 
 const notesLinks = [
