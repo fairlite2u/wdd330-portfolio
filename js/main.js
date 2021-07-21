0638e6ff2ef79716c29dce276af9cfa84482e129
fairlite2u <pyl95001@byui.edu>
2021-07-21 15:43:33 -0600

    url link address fix

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 const weekLinks = [
-    {label: "Week 1", url: "week1/index.html"},
-    {label: "Week 2", url: "week2/index.html"},
-    {label: "Week 3", url: "week3/index.html"}, 
-    {label: "Week 4", url: "week4/index.html"},
-    {label: "Week 5", url: "week5/index.html"},
-    {label: "Midterm: Todo App", url: "todo/index.html"},
-    {label: "Week 7", url: "week7/index.html"},
-    {label: "Week 8", url: "week8/index.html"},
-    {label: "Week 9", url: "week9/index.html"},
-    {label: "Week 10", url: "week10/index.html"},
-    {label: "Week 11", url: "week11/index.html"},
-    {label: "Practice Tutorials", url: "tutorials/index.html"},
-    {label: "Final Project: Costume Inventory App", url: "costumeapp/index.html"}
+    {label: "Week 1", url: "week1/"},
+    {label: "Week 2", url: "week2/"},
+    {label: "Week 3", url: "week3/"}, 
+    {label: "Week 4", url: "week4/"},
+    {label: "Week 5", url: "week5/"},
+    {label: "Midterm: Todo App", url: "todo/"},
+    {label: "Week 7", url: "week7/"},
+    {label: "Week 8", url: "week8/"},
+    {label: "Week 9", url: "week9/"},
+    {label: "Week 10", url: "week10/"},
+    {label: "Week 11", url: "week11/"},
+    {label: "Practice Tutorials", url: "tutorials/"},
+    {label: "Final Project: Costume Inventory App", url: "costumeapp/"}
 ]
 
 const notesLinks = [
